f8e92769a8074bf98f3c40ad9b2e7d11c4272431
yahei1232 <yaheiyahei693@gmail.com>
2023-03-14 21:39:13 +0300

    create port connect in index file in backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const cors = require('cors');
+
 require("./db/db")
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
