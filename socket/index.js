9415b3b76d63181ffde056dbe9ce44ad53ec4b00
yahei1232 <yaheiyahei693@gmail.com>
2023-03-19 14:28:33 +0300

    add addUser function and addUser on socket in socket

--- socket/index.js
+++ socket/index.js
@@ -4,9 +4,23 @@ const io = require("socket.io")(8900, {
     }
 })
 
+let users = [];
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+        users.push({ userId, socketId });
+};
+
 io.on("connection", (socket) => {
     //when ceonnect
     console.log("a user connected.");
+
+    //take userId and socketId from user
+    socket.on("addUser", (userId) => {
+        console.log(userId);
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    });
 
     //when disconnect
     socket.on("disconnect", () => {
